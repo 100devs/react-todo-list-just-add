626ad6d9b3fd68c885ca39bcb17ab1f584835e25
Imran Mohamed <71926855+i786m@users.noreply.github.com>
2021-06-03 22:32:21 +0100

    added comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+// imports useState functionality, Header,AddToDo and List components
 import { useState } from 'react'
 import Header from "./components/Header";
 import List from "./components/List"
 import AddTodo from "./components/AddTodo"
 
+//main app function to generate the components which are rendered in the root div in the index.html file
 function App() {
-
+// setting the initial state of the toDos to the array of todo objects
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -19,13 +21,13 @@ function App() {
       item: "Exercise",
     },
   ]);
-
+//when a new to do is made an id is generated for the id, this is then passed into the set state which updates the setstate array to add the new todo to the existing ones
   const addTodo = (todo) => {
     const id = Math.ceil(Math.random()*100000)
     const newTodo = {id, ...todo}
     setTodos([...todos, newTodo])
   }
-
+//genereates the html thats rendered into the html by using the components imported at the top of the page to dynamically generate the html required based on the todos we have
   return (
     <div className="container">
       <Header title="Todo List" />
@@ -34,5 +36,5 @@ function App() {
     </div>
   );
 }
-
+// maves the App function available throughout the app througgh the use of import in other files
 export default App;
