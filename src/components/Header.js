f4273d4ff775173c47084e9e756977a5719bd6c1
KatKava <katryn.kav97@gmail.com>
2021-06-03 19:53:47 +0100

    added comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from "react";
+//importing React 
 
 const Header = (props) => {
+  //We're creating our header component, function-based component
+  //passing in our props  
   return (
     <div>
       <h1 style={headerStyle}>{props.title}</h1>
     </div>
-  );
+  ); //headerStyle and props.title are the props we're pulling in. headerStyle object, we're using it to style our header 
 };
 
 const headerStyle = {
   fontSize: "40px",
   textDecoration: "underline",
+  //these are my props:  fontSize and textDecoration 
+  //textDecoration -this is camelCase so we're writing JSX 
+  //some custom CSS going on here with out headerStyle object
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from 'react'; 
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//above we're importing important depencies 
 
 ReactDOM.render(
+  //ReactDOM is rendering stuff to the actual DOM. 
+  //takes the react component and actually add it to the DOM 
+  //React.StrictMode is a tool for highlighting potential problems in an application. It doesn't render any visible UI. 
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+  //taking the App component and rendering into the root element in the HTML 
+  //App component is the thing being put in browser, so everything should go into root. App is universal! Taking this 1 component that has everything in it and placing it into the HTML file 
 );
 
 // If you want to start measuring performance in your app, pass a function
