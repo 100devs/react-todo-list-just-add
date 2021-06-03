9798674013de4bca7a0ca14b411dc12e4dc786cf
Imran Mohamed <71926855+i786m@users.noreply.github.com>
2021-06-03 22:17:50 +0100

    added comments

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,12 @@
-
+//function to render the Todo component div, passed todo as a prop
 const Todo = ({ todo }) => {
     return (
         <div>
+        //using the prop property to set the text of the h2
             <h2>{todo.item}</h2>
         </div>
     )
 }
-
+// ennable the use of the todo component in other files
 export default Todo
 
