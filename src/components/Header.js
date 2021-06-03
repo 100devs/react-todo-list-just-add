2a8f062bcaf32fc4fba8d1707b8e44876dad224b
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-06-03 18:10:08 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
-
+//Header component with props passed
 const Header = (props) => {
   return (
     <div>
+    //props.title  is the props for the title and the headerStyle is an object that has properties used 
+    //to style the header
+    
       <h1 style={headerStyle}>{props.title}</h1>
     </div>
   );
 };
-
+//property Names are written in camelCase as it is a JSX format
 const headerStyle = {
   fontSize: "40px",
   textDecoration: "underline",
