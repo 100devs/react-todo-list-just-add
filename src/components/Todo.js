265b2770010fcf37ed2d495a6e8927783598b3f3
Memphis83 <kiran.iyer83@gmail.com>
2021-06-04 01:59:47 +0530

    comments added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,8 @@
-
+// creating a Todo funciton component that is receing "todo object" as props from the top level (APP)
 const Todo = ({ todo }) => {
     return (
         <div>
+        {/* receiving todo items from the todo object in props */}
             <h2>{todo.item}</h2>
         </div>
     )
