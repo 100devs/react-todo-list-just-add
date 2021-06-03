547e3b574c9df5351310dd1418bd39b8b5c9160e
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-06-03 18:25:00 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
+//importing all the necessary files and dependencies
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//ReactDOM renders stuff to the DOM
 ReactDOM.render(
+  //StrictMode is a tool for highlighting potential problems in an application.
+  //StrictMode does not render any visible UI.
+  //It activates additional checks and warnings for its descendants.
   <React.StrictMode>
+  //App is used to output everything in the browser.
     <App />
   </React.StrictMode>,
+  //a “root” DOM node because everything inside it will be managed by React DOM.
+//Applications built with just React usually have a single root DOM node. 
+ // If you are integrating React into an existing app, you may have as many isolated root
+  //DOM nodes as you like.
+  
   document.getElementById('root')
 );
 
