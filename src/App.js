183d744a0e2b99c38e921dcd38c86716bdad4e2e
khafrewatkins <khafrewatkins@gmail.com>
2021-06-03 20:34:10 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import { useState } from 'react' // allows use of State without Class-based components
 import Header from "./components/Header";
 import List from "./components/List"
 import AddTodo from "./components/AddTodo"
 
 function App() {
-
+  // controlling the root of the whole app 
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -19,20 +19,30 @@ function App() {
       item: "Exercise",
     },
   ]);
-
+{/* ^^ global state, the base level for the initial state
+// useState is a React hook (uses array desstructuring)
+// todos is state, setTodos changes state */}
   const addTodo = (todo) => {
     const id = Math.ceil(Math.random()*100000)
+    {/* const newTodo allows randomly generated id and pairs item(new todo). Uses spread opperator to put new todo at and of array(?) (spread opperator may not be necessary do to creating ONE todo at a time) */}
     const newTodo = {id, ...todo}
+    {/* adds new todo in array of todos list */}
     setTodos([...todos, newTodo])
   }
 
   return (
     <div className="container">
       <Header title="Todo List" />
+       {/* header component to render out the header */}
       <AddTodo onAdd={addTodo} />
+      {/* AddTodo component allows us to add new todos */}
       <List todos={todos}/>
+      {/* list component for rendering the List */}
+      {/* passing todos into the list component */}
     </div>
   );
 }
 
-export default App;
+export default App; {/* allows other files to see our App function */}
+
+
