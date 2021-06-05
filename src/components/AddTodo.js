f6c254bf10a31f66871950faf8a807000c22c3ef
Kieran <60425579+Memphis1983@users.noreply.github.com>
2021-06-05 10:09:09 +0530

    Added comments

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,16 +1,23 @@
+// importing usestate hook from react
 import { useState } from 'react'
 
+// creating Addtodo component which takes one param, we are directly accessing the onAdd event from the top level as param
 const AddTodo = ({ onAdd }) => { 
+//    create two state variables using destructuring for useState hook 
     const [item, setItem] = useState('')
 
+//     Create handler function for our onSubmit event in the form element
     const onSubmit = (e) => {
+//         prevents the default refresh 
         e.preventDefault()
+//         calling our onAdd handler function and passing our item as an object
         onAdd({item})
-
+// resetting our item state back to empty
         setItem('')
     }
 
     return (
+//         adding onSubmit event
         <form onSubmit={onSubmit}>
             <label>Add Todo:</label>
             <input 
