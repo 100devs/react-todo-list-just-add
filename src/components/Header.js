2dfaeb4f1a8b2c093856020ff5c1d6cfeafb99b2
Kieran <60425579+Memphis1983@users.noreply.github.com>
2021-06-05 10:04:30 +0530

    added comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
+// importing react
 import React from "react";
 
+// creating a Header component which takes one param - props
 const Header = (props) => {
   return (
     <div>
+//     we are fetching styles from the variable created below 
       <h1 style={headerStyle}>{props.title}</h1>
     </div>
   );
 };
 
+// we are creating a new object with styles for our component
 const headerStyle = {
   fontSize: "40px",
   textDecoration: "underline",
