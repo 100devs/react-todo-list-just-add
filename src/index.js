e6246ee33b97b61b9ca48fddde84bfe2c5c676fb
Kieran <60425579+Memphis1983@users.noreply.github.com>
2021-06-05 09:50:13 +0530

    Added comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+// importing react module from React
 import React from 'react';
+// importing reactdom which gives us access to virtual dom
 import ReactDOM from 'react-dom';
+// importing styles 
 import './index.css';
+// importing App component
 import App from './App';
+// importing idk what it is lol
 import reportWebVitals from './reportWebVitals';
 
+// this is a method to render react app to the webpage it takes in two arguments html code and an html element, 
+// The purpose of the function is to display the specified HTML code inside the specified HTML element.
 ReactDOM.render(
   <React.StrictMode>
     <App />
