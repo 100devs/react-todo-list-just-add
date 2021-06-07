e4e39e8b3de591ada512da3549d51940de32e96e
Jonathan Yau <jonathankyau@gmail.com>
2021-06-07 15:35:03 -0700

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,14 @@
-import Todo from './Todo'
+import Todo from './Todo' //import Todo component
 
-const List = ({ todos }) => {
+const List = ({ todos }) => { //List component with todos parameter
     return (
         <div>
-          {todos.map((todo) => 
-            <Todo key={todo.id} todo={todo} />
+          {todos.map((todo) => //display each todo
+            <Todo key={todo.id} todo={todo} /> //Todo component will have id and array of todos
           )}  
         </div>
     )
 }
 
-export default List
+export default List //export List component
 
