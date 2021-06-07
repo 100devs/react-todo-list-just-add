4f3c33a64cc7584f79e2485aa05e1ac6acd3b7fc
Jonathan Yau <jonathankyau@gmail.com>
2021-06-07 15:35:59 -0700

    added comments

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,11 @@
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo }) => { //create Todo component with todo parameter
     return (
         <div>
-            <h2>{todo.item}</h2>
+            <h2>{todo.item}</h2> 
         </div>
     )
 }
 
-export default Todo
+export default Todo //export Todo component
 
